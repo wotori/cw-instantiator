9678164a1302b3478f448e916d1b76071605f6a3
Wotori Movako <10486621+wotori@users.noreply.github.com>
2023-04-22 13:00:34 +0300

    code error.rs fixes for building

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,20 @@
-#[derive(Debug)]
+use cosmwasm_std::StdError;
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
-    ContractInstantiationFailed,
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+
+    #[error("token_id already claimed")]
+    Claimed {},
+
+    #[error("Cannot set approval that is already expired")]
+    Expired {},
+
+    #[error("Approval not found for: {spender}")]
+    ApprovalNotFound { spender: String },
 }
